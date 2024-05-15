812e8238edec07af2379077b4669606f34c231dc
chonkov <georgi.chonkov27@gmail.com>
2024-05-16 01:18:14 +0300

    Made minor changes

--- src/Presale.sol
+++ src/Presale.sol
@@ -1,4 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract Presale {}
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+struct PresaleMetadata {
+    string name;
+    string symbol;
+    bytes websiteURL;
+    bytes docsURL;
+    bytes presaleInfoURL;
+}
+
+contract Presale is Ownable, ERC20 {
+    error InvalidETHAmount(uint256 amount);
+
+    event PresaleTokensBought(address indexed buyer, uint256 amount, uint256 value);
+
+    bytes public websiteURL;
+    bytes public docsURL;
+    bytes public presaleInfoURL;
+
+    mapping(address => uint256) public claimableAmounts;
+    uint256 initSupply;
+    uint256 price;
+
+    constructor(address _owner, PresaleMetadata memory _presaleMetadata, uint256 _initSupply, uint256 _price)
+        Ownable(_owner)
+        ERC20(_presaleMetadata.name, _presaleMetadata.symbol)
+    {
+        websiteURL = _presaleMetadata.websiteURL;
+        docsURL = _presaleMetadata.docsURL;
+        presaleInfoURL = _presaleMetadata.presaleInfoURL;
+
+        initSupply = _initSupply;
+        price = _price;
+    }
+
+    function buyPresaleToken(uint256 _amount) external payable {
+        if (msg.value != _amount * price) revert InvalidETHAmount(msg.value);
+
+        claimableAmounts[msg.sender] += _amount;
+        initSupply -= _amount;
+
+        emit PresaleTokensBought(msg.sender, _amount, msg.value);
+    }
+
+    function buyToken() external {}
+
+    function isStarted() external view returns (bool) {}
+    function isEnded() external view returns (bool) {}
+    function isClaimable() external view returns (bool) {}
+
+    //  function transferOperatorOwnership(address newOperator) external;
+    //  function updateWhitelist(uint256 _wlBlockNumber, uint256 _wlMinBalance, bytes32 _wlRoot) external;
+    //  function increaseHardCap(uint256 _tokenHardCapIncrement) external;
+}
---
+++ src/PresaleFactory.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+import {Presale, PresaleMetadata} from "./Presale.sol";
+
+contract PresaleFactory is Ownable {
+    enum PresaleStatus {
+        NonExistent,
+        Started,
+        Ended
+    }
+
+    error InvalidProtocolFee(uint256 protocolFee);
+    error InvalidProtocolFeeAddress(address protocolFeeAddress);
+
+    event ProtocolFeeSet(uint256 previousProtocolFee, uint256 newProtocolFee);
+    event ProtocolFeeAddressTransferred(address previousProtocolFeeAddress, address newProtocolFeeAddress);
+    event PresaleCreated(address indexed presale, address indexed creator);
+
+    uint256 public protocolFee;
+    address public protocolFeeAddress;
+
+    mapping(Presale => PresaleStatus) public presaleStatus;
+
+    // Constructor
+    constructor(address _owner, uint256 _protocolFee, address _protocolFeeAddress) Ownable(_owner) {
+        protocolFee = _protocolFee;
+        protocolFeeAddress = _protocolFeeAddress;
+    }
+
+    // External functions
+    function createPresale(
+        address _owner,
+        PresaleMetadata calldata _presaleMetadata,
+        uint256 _initSupply,
+        uint256 _price
+    ) external {
+        Presale presale = new Presale(_owner, _presaleMetadata, _initSupply, _price);
+        presaleStatus[presale] = PresaleStatus.Started;
+
+        emit PresaleCreated(address(presale), msg.sender);
+    }
+
+    function setProtocolFee(uint256 newProtocolFee) external onlyOwner {
+        if (newProtocolFee == 0) revert InvalidProtocolFee(0);
+
+        emit ProtocolFeeSet(protocolFee, newProtocolFee);
+
+        protocolFee = newProtocolFee;
+    }
+
+    function setProtocolFeeAddress(address newProtocolFeeAddress) external onlyOwner {
+        if (newProtocolFeeAddress == address(0)) revert InvalidProtocolFeeAddress(address(0));
+
+        emit ProtocolFeeAddressTransferred(protocolFeeAddress, newProtocolFeeAddress);
+
+        protocolFeeAddress = newProtocolFeeAddress;
+    }
+}
